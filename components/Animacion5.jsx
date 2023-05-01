fafd994e3d8b6044884775bcebd640f7dd7a3179
Diana Rut <93566502+DIANARUTGARCIA@users.noreply.github.com>
2023-05-01 09:15:28 -0600

    Animación con Spring

--- components/Animacion5.jsx
+++ components/Animacion5.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import {Text, TouchableWithoutFeedback, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  StyleSheet,
+  Animated,
+} from 'react-native';
 
 const Animacion5 = () => {
+  const [animacion] = useState(new Animated.Value(1));
+
+  const presionarBtn = () => {
+    Animated.spring(animacion, {
+      toValue: 0.8,
+       useNativeDriver: false,
+    }).start();
+  };
+  const soltarB = () => {};
+
+  const estiloAnimacion = {
+    transform: [{scale: animacion}],
+  };
   return (
     <View style={styles.contenedor}>
-      <TouchableWithoutFeedback>
-        <View style={styles.btn}>
+      <TouchableWithoutFeedback
+        onPressIn={() => presionarBtn()}
+        onPressOut={() => soltarB()}
+      >
+        <Animated.View style={[styles.btn, estiloAnimacion]}>
           <Text style={styles.texto}>Iniciar Sesión</Text>
-        </View>
+        </Animated.View>
       </TouchableWithoutFeedback>
     </View>
   );
@@ -15,9 +37,21 @@ const Animacion5 = () => {
 
 const styles = StyleSheet.create({
   contenedor: {
-    marginTop: 40,
+    alignItems: 'center',
   },
-  texto: {},
-  btn:{},
+  texto: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    fontSize: 24,
+  },
+  btn: {
+    backgroundColor: '#00AEE2',
+    width: 280,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+  },
 });
 export default Animacion5;
